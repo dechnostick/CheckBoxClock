216367fab442fa11299170b68122f5276e8168a0
dechnostick <etherealmaro@gmail.com>
2014-08-08 22:31:14 +0900

    アセンブリ名を変更

--- CheckBoxClock/My Project/Settings.Designer.vb
+++ CheckBoxClock/My Project/Settings.Designer.vb
@@ -63,10 +63,10 @@ Namespace My
      Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()>  _
     Friend Module MySettingsProperty
         
-        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.WindowsApplication1.My.MySettings
+        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")> _
+        Friend ReadOnly Property Settings() As Global.CheckBoxClock.My.MySettings
             Get
-                Return Global.WindowsApplication1.My.MySettings.Default
+                Return Global.CheckBoxClock.My.MySettings.Default
             End Get
         End Property
     End Module
